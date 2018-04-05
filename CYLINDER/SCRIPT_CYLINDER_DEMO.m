7203da94b5499101a61a79b71ad4c5eaadc56ecc
FABRE David <fabred@stevens.imft.fr>
2018-04-05 16:34:30 +0200

    fix

--- CYLINDER/SCRIPT_CYLINDER_DEMO.m
+++ CYLINDER/SCRIPT_CYLINDER_DEMO.m
@@ -16,6 +16,7 @@ disp(' LARGE MESH : [-40:80]x[0:40] ');
 disp(' ');    
 bf=SF_Init('Mesh_Cylinder.edp',[-40 80 40]);
 plotFF(bf,'mesh');
+pause(0.01);
 
 bf=SF_BaseFlow(bf,'Re',1);
 bf=SF_BaseFlow(bf,'Re',10);
@@ -24,20 +25,28 @@ bf=SF_Adapt(bf);
 
 bf
 plotFF(bf,'ux');
+pause(0.01);
 
 disp(' ');
 
 disp('Eigenvalue computation : compute 10 modes and plot the spectrum')
 [ev,em] = SF_Stability(bf,'shift',0.04+0.76i,'nev',10,'type','D');
+ev
 
 disp('Eigenvalue computation : compute 1 mode, including structural sensitivity')
 [ev,em] = SF_Stability(bf,'shift',0.04+0.76i,'nev',1,'type','S');
 
+
 disp('mesh adaptation to SENSITIVITY and new computation of eigenvalues on improved mesh : ')
 bf = SF_Adapt(bf,em);
-[ev,em] = SF_Stability(bf,'shift',0.04+0.76i,'nev',1,'type','S','plotspectrum','yes');
+[ev,em] = SF_Stability(bf,'shift',0.04+0.76i,'nev',1,'type','S');
+ev
 plotFF(em,'ux1');
 plotFF(em,'sensitivity');
+pause(0.01);
+
+[ev,em] = SF_Stability(bf,'shift',0.04+0.76i,'nev',1,'type','D','plotspectrum','yes');
+
 
 
 %%% DETERMINATION OF THE INSTABILITY THRESHOLD
