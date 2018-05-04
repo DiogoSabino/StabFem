8c442d2b14627a0485214beed3517d4e7f29fe77
FERREIRA-SABINO Diogo <dferreir2@garfunkel.imft.fr>
2018-05-04 14:51:49 +0200

    finally...

--- ACOUSTICS_PIPES/SF_Start.m
+++ ACOUSTICS_PIPES/SF_Start.m
@@ -1,6 +1,22 @@
 global ff ffdir ffdatadir sfdir verbosity
-%ff = '/PRODCOM/FREEFEM/Ubuntu12.04/3.29/bin/FreeFem++-nw'; % on IMFT network
-ff = '/usr/local/ff++/openmpi-2.1/3.55/bin/FreeFem++-nw'; % on DF's macbookpro 
+
+
+
+if(isunix)
+    ff =  '/usr/local/bin/FreeFem++';   % on most systems, including the server AZTECA (UPS)
+    %ff = '/PRODCOM/FREEFEM/Ubuntu12.04/3.29/bin/FreeFem++-nw'; % on IMFT network
+end
+
+if(ismac)
+    ff =  '/usr/local/bin/FreeFem++'; % on most mac instalations
+    %ff = '/usr/local/ff++/openmpi-2.1/3.55/bin/FreeFem++-nw'; % on DF's macbookpro 
+end
+   
+if(ispc)
+    ff = 'launchff++'; % for windows systems
+end
+
+
 ffdatadir = './';
 %sfdir = '/Users/dfabre/StabFem/SOURCES_MATLAB/'; % where to find the matlab drivers
 sfdir = './';
@@ -10,4 +26,5 @@ verbosity = 5;
 %if(exist(ffdatadir)~=7)
 %    mysystem(['mkdir ' ffdatadir]);
 %end
-mysystem(['echo "// File automatically created by StabFem" > SF_Geom.edp']); % a file SF_Geom should be present, even if blank 
+mysystem(['echo "// File automatically created by StabFem" > SF_Geom.edp']); 
+% a file SF_Geom should be present, even if blank 
